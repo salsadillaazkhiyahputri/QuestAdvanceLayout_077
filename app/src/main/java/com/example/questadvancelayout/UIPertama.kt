2365cf51119f62a3bf6e467d4252f5c011438f7e
salsadillaazkhiyahputri <salsadillaazkhiyahputri@gmail.com>
2025-10-23 12:49:37 +0700

    menambahkan spacer

--- app/src/main/java/com/example/questadvancelayout/UIPertama.kt
+++ app/src/main/java/com/example/questadvancelayout/UIPertama.kt
@@ -70,3 +70,4 @@ fun ActivitasPertama(modifier: Modifier){
                                 contentDescription = null,
                                 modifier = Modifier.size(100.dp).padding(5.dp)
                             )
+                            Spacer(modifier = Modifier.width(30.dp))
