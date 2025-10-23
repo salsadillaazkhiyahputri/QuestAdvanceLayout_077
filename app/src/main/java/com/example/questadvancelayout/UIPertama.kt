27ffc49695798f37f37be83e52b638e2d9c2b5c2
salsadillaazkhiyahputri <salsadillaazkhiyahputri@gmail.com>
2025-10-23 12:46:06 +0700

    menambahkan dan membuat card

--- app/src/main/java/com/example/questadvancelayout/UIPertama.kt
+++ app/src/main/java/com/example/questadvancelayout/UIPertama.kt
@@ -38,3 +38,9 @@ fun ActivitasPertama(modifier: Modifier){
             fontSize= 20.sp,
             fontWeight = FontWeight.Bold)
         Spacer(modifier = Modifier.height(25.dp))
+        Card(modifier = Modifier
+            .fillMaxWidth(1f)
+            .padding(12.dp),
+            colors = CardDefaults.cardColors(
+                containerColor = Color.DarkGray
+            )){
