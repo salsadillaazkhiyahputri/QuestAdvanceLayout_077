6fc95b33259d2ab12d15bf90be85ab361706967c
salsadillaazkhiyahputri <salsadillaazkhiyahputri@gmail.com>
2025-10-23 12:51:58 +0700

    menambahkan layout alamat

--- app/src/main/java/com/example/questadvancelayout/UIPertama.kt
+++ app/src/main/java/com/example/questadvancelayout/UIPertama.kt
@@ -79,3 +79,12 @@ fun ActivitasPertama(modifier: Modifier){
                                     color = Color.White,
                                     modifier = Modifier.padding(top=15.dp)
                                 )
+                                    Text(
+                                        stringResource(R.string.alamat),
+                                        fontSize = 20.sp,
+                                        color = Color.Yellow,
+                                        modifier = Modifier.padding(top=10.dp)
+                                    )
+                                }
+                            }
+                        }
