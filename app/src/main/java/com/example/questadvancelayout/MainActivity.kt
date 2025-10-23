408d032819dd694007d1f0ca704ff8e23d7fe759
salsadillaazkhiyahputri <salsadillaazkhiyahputri@gmail.com>
2025-10-23 13:00:06 +0700

    mengubah mainactiviy

--- app/src/main/java/com/example/questadvancelayout/MainActivity.kt
+++ app/src/main/java/com/example/questadvancelayout/MainActivity.kt
@@ -20,8 +20,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             QuestAdvanceLayoutTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    ActivitasPertama(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -30,18 +29,3 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    QuestAdvanceLayoutTheme {
-        Greeting("Android")
-    }
-}
